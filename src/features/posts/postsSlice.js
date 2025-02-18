f88b50aacd4d958b69c85ccbeff4c764cb644936
varun <hbeun8@outlook.com>
2025-02-18 14:27:41 +0530

    added redux slice

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,91 @@
+const initialState = [
+        { id: 0, text: 'Learn React', read: true, favorite: true, color:'', author:'', subreddit:'', type:'' },
+        { id: 1, text: 'Learn Redux', read: false, favorite: false, color: 'purple', author:'', subreddit:'', type:'' },
+        { id: 2, text: 'Build something fun!', read: false, favorite: false, color: 'blue', author:'', subreddit:'', type:'' }
+]
+
+function nextPostId(posts) {
+    const maxId = posts.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+    return maxId + 1
+}
+
+export default function appReducer(state = initialState, action) {
+    // The reducer normally looks at the action type field to decide what happens
+    switch (action.type) {
+      // Do something here based on the different types of actions
+      case 'posts/textAdded': {
+        // We need to return a new state object
+        return {
+          // that has all the existing state data
+          ...state,
+          // but has a new array for the `todos` field
+          posts: [
+            // with all of the old todos
+            ...state.posts,
+            // and the new todo object
+            {
+              // Use an auto-incrementing numeric ID for this example
+              id: nextPostId(state.posts),
+              text: action.payload,
+              read: false,
+              favorite: false,
+              color: '',
+              author: '',
+              subreddit: '',
+              type: ''
+            }
+          ]
+        }
+      }
+      case 'posts/readToggled': {
+        return {
+          // Again copy the entire state object
+          ...state,
+          // This time, we need to make a copy of the old posts array
+          posts: state.posts.map(post => {
+            // If this isn't the todo item we're looking for, leave it alone
+            if (post.id !== action.payload) {
+              return post
+            }
+            // We've found the post that has to change. Return a copy:
+            return {
+              ...post,
+              // Flip the read flag
+              read: !post.read
+            }
+          })
+        }
+      }
+      //this is the template for changes
+      case 'posts/colorAdded': {
+        return {
+          // Copy the whole state
+          ...state,
+          // Overwrite the color value
+          colors: {
+            ...state.colors,
+            // Add the new color
+            status: action.payload
+            }
+          }
+        }
+        //this is the template for changes
+        case 'posts/favoriteAdded': {
+          return {
+            // Copy the whole state
+            ...state,
+            // Overwrite the color value
+            favorite: {
+              ...state.favorite,
+              // Add the new color
+              status: action.payload
+              }
+            }
+        }
+        
+      default:
+        // If this reducer doesn't recognize the action type, or doesn't
+        // care about this specific action, return the existing state unchanged
+        return state
+    }
+  }
--- src/reducer.js
+++ src/reducer.js
@@ -1,79 +1,21 @@
-const initialState = {
-//these will be dynamically populated
-  posts: [
-    { id: 0, text: 'Learn React', read: true, favorite: true, color:'', author:'', subreddit:'', type:'' },
-    { id: 1, text: 'Learn Redux', read: false, favorite: false, color: 'purple', author:'', subreddit:'', type:'' },
-    { id: 2, text: 'Build something fun!', read: false, favorite: false, color: 'blue', author:'', subreddit:'', type:'' }
-  ],
-//these will be dynamically populated
-  filters: {
-    status: 'Active',
-    favorite: [true, false],
-    read: [true, false],
-    colors: ['red', 'blue'],
-    authors: ['author1', 'author2'],
-    subreddits: ['subreddit1', 'subreddit2'],
-    types: ['type1', 'type2']
-  }
-}
+import { combineReducers } from 'redux'
 
-function nextPostId(posts) {
-  const maxId = posts.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
-  return maxId + 1
-}
+import postsReducer from './features/posts/postsSlice'
+import filtersReducer from './features/filters/filtersSlice'
 
-// Use the initialState as a default value
-export default function appReducer(state = initialState, action) {
-  // The reducer normally looks at the action type field to decide what happens
-  switch (action.type) {
-    // Do something here based on the different types of actions
-    case 'posts/textAdded': {
-      // We need to return a new state object
-      return {
-        // that has all the existing state data
-        ...state,
-        // but has a new array for the `todos` field
-        posts: [
-          // with all of the old todos
-          ...state.posts,
-          // and the new todo object
-          {
-            // Use an auto-incrementing numeric ID for this example
-            id: nextPostId(state.posts),
-            text: action.payload,
-            read: false,
-            favorite: false,
-            color: '',
-            author: '',
-            subreddit: '',
-            type: ''
-          }
-        ]
-      }
-    }
-    case 'posts/postToggled': {
-      return {
-        // Again copy the entire state object
-        ...state,
-        // This time, we need to make a copy of the old todos array
-        posts: state.posts.map(post => {
-          // If this isn't the todo item we're looking for, leave it alone
-          if (post.id !== action.payload) {
-            return post
-          }
+const rootReducer = combineReducers({
+    // Define a top-level state field named `todos`, handled by `todosReducer`
+    posts: postsReducer,
+    filters: filtersReducer
+})
 
-          // We've found the todo that has to change. Return a copy:
-          return {
-            ...post,
-            // Flip the completed flag
-            read: !post.read
-          }
-        })
-      }
-    }
-    default:
-      // If this reducer doesn't recognize the action type, or doesn't
-      // care about this specific action, return the existing state unchanged
-      return state
-  }
-}
+/*
+combineReducers accepts an object where the key names will
+become the keys in your root state object, and the values are
+the slice reducer functions that know how to update those 
+slices of the Redux state.
+
+Remember, the key names you give to combineReducers decides 
+what the key names of your state object will be!
+*/
+export default rootReducer;
